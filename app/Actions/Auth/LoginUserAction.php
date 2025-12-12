84fc36bcd304f65faf03636f94de3e39e146810a
Julien Legrand <julien@nomala.com>
2025-12-12 09:28:49 +0100

    Add debug

--- app/Actions/Auth/LoginUserAction.php
+++ app/Actions/Auth/LoginUserAction.php
@@ -3,6 +3,8 @@
 namespace App\Actions\Auth;
 
 use App\Services\CognitoService;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class LoginUserAction
 {
@@ -14,12 +16,13 @@ class LoginUserAction
      */
     public function handle(array $credentials): array
     {
-        //        try {
-        return $this->cognitoService->login($credentials);
-        //        } catch (Exception $e) {
-        //            \Log::error($e->getMessage());
-        //
-        //            return ['error' => $e->getMessage()];
-        //        }
+        Log::info('LoginUserAction');
+        try {
+            return $this->cognitoService->login($credentials);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+
+            return ['error' => $e->getMessage()];
+        }
     }
 }
